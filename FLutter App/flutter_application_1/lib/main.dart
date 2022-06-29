0446bc58011e11fdcb78be6f04e5b5f94b9a15ba
pankkap <pankkap@gmail.com>
2022-06-29 17:38:37 +0530

    Day-24 Code

--- FLutter App/flutter_application_1/lib/main.dart
+++ FLutter App/flutter_application_1/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter_application_1/12-MyTabBar1.dart';
 import 'package:flutter_application_1/13-DrawerDemo.dart';
 import 'package:flutter_application_1/15-Selectable.dart';
 import 'package:flutter_application_1/9-BottomNavigationBarDemo.dart';
+import 'package:flutter_application_1/AnimateScreen2.dart';
 import 'package:flutter_application_1/SI_Calculator.dart';
 import 'package:flutter_application_1/Screens/HomeScreen.dart';
 import 'package:flutter_application_1/Screens/Screen1.dart';
@@ -14,9 +15,14 @@ import 'package:flutter_application_1/Screens/Screen2.dart';
 
 import '14-DataTableDemo.dart';
 import '16-TextWidgetsDemo.dart';
+import 'AnimateWidget.dart';
+import 'BottomSheetDemo.dart';
 import 'PizzaApp.dart';
 import 'SI1.dart';
 import 'Screens/Screen0.dart';
+import 'StackDemo.dart';
+import 'WebViewDemo.dart';
+import 'flutterCrud.dart';
 
 void main() {
   runApp(
@@ -25,14 +31,19 @@ void main() {
       // home: BottomNavigationDemo(),
       // home: MyTabBar(),
       // home: MyTabBar1(),
-      // home: SelectableDemo(),
-      // home: ListTileDemo(),
       // home: DrawerDemo(),
+      // home: ListTileDemo(),
       // home: DataTableDemo(),
+      // home: SelectableDemo(),
       // home: TextFieldDemo(),
-      // home: SI_Calculator(),
-      home: PizzaApp(),
       // home: sicalci(),
+      // home: PizzaApp(),
+      // Day-23
+      // home: AnimateWidget(),
+      // home: WebViewDemo(),
+      // home: BottomSheetDemo(),
+      // home: StackDemo(),
+      home: FlutterCrud(),
 
       routes: {
         'S0': (context) => Screen0(title: "Settings"),
